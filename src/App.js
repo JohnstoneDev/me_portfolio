7ab24eda6798b673a34514abda14be7e2d0c3ed7
JohnstoneDev <marucha.webdev@yahoo.com>
2022-12-22 19:24:54 +0300

    Content for Dev mode

--- src/App.js
+++ src/App.js
@@ -2,18 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header bg-black bg-blend-darken">
-        <h1 className='text-4xl text-slate-300 animate-pulse'> React Template Repository with the following packages Pre-installed :</h1>      
-        <ol className='pt-5'>
-           <li>React Router (v5) </li>
-           <li>Tailwind CSS </li>
-            <li> Configuration to use a proxy server.</li>
-            <li>React start script modified to use openssl legacy provider.</li>
-        </ol>
-      
-      </header>
-    </div>
+    <div className="App text-slate-300 bg-slate-900 font-mono min-h-screen font-semibold">
+        <section className='flex flex-col items-center justify-center p-8 m-0 gap-4'>
+          <h1 className='text-4xl animate-pulse text-'>Portfolio Under Maintenance</h1>
+          <iframe src="https://giphy.com/embed/xUPJPdEApVwRVwnPIA" height="500" width="500" className="p-2 rounded-lg" class="giphy-embed" allowFullScreen title="bob the builder"></iframe>
+        </section>
+      </div>
   );
 }
 

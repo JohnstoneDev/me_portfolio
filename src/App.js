39147ba46a3f5ae37cd1814b2ab06d79a4c54c84
JohnstoneDev <marucha.webdev@yahoo.com>
2022-12-22 19:40:30 +0300

    Fix gif height

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App text-slate-300 bg-slate-900 font-mono min-h-screen font-semibold">
+    <div className="App text-slate-300 bg-slate-900 font-mono min-h-screen min-w-screen font-semibold">
         <section className='flex flex-col items-center justify-center p-8 m-0 gap-4'>
           <h1 className='text-4xl animate-pulse text-'>Portfolio Under Maintenance</h1>
-          <iframe src="https://giphy.com/embed/xUPJPdEApVwRVwnPIA" height="500" width="500" className="p-2 rounded-lg" class="giphy-embed" allowFullScreen title="bob the builder"></iframe>
+          <iframe src="https://giphy.com/embed/xUPJPdEApVwRVwnPIA" height="450" width="400" className="p-2 rounded-lg object-cover" class="giphy-embed" allowFullScreen title="bob the builder"></iframe>
         </section>
       </div>
   );

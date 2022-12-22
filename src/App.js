708ed2a149ad47a90d4a0d582e08e614a2bc7b9b
JohnstoneDev <marucha.webdev@yahoo.com>
2022-12-22 19:47:30 +0300

    Add Styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App text-slate-300 bg-slate-900 font-mono min-h-screen min-w-screen font-semibold">
         <section className='flex flex-col items-center justify-center p-2 m-0 gap-4'>
           <h1 className='text-4xl animate-pulse text-'>Portfolio Under Maintenance</h1>
-          <iframe src="https://giphy.com/embed/xUPJPdEApVwRVwnPIA" height="450" width="400" className="p-2 rounded-lg object-cover" class="giphy-embed" allowFullScreen title="bob the builder"></iframe>
+          <iframe src="https://giphy.com/embed/xUPJPdEApVwRVwnPIA" height="450" width="400" className="rounded-lg object-cover" class="giphy-embed" allowFullScreen title="bob the builder"></iframe>
         </section>
       </div>
   );

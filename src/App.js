603e1e66724f967e1a262c06d71ef1a06059cac3
JohnstoneDev <marucha.webdev@yahoo.com>
2022-12-22 19:44:27 +0300

    Fix padding

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App text-slate-300 bg-slate-900 font-mono min-h-screen min-w-screen font-semibold">
-        <section className='flex flex-col items-center justify-center p-8 m-0 gap-4'>
+        <section className='flex flex-col items-center justify-center p-2 m-0 gap-4'>
           <h1 className='text-4xl animate-pulse text-'>Portfolio Under Maintenance</h1>
           <iframe src="https://giphy.com/embed/xUPJPdEApVwRVwnPIA" height="450" width="400" className="p-2 rounded-lg object-cover" class="giphy-embed" allowFullScreen title="bob the builder"></iframe>
         </section>

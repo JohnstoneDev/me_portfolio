725b4714aba0d311881faec378a0da2979055244
Marucha <marucha.webdev@yahoo.com>
2023-10-16 22:03:40 +0300

    chore: Setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import './App.css';
+import './Styles/App.css'
 
 function App() {
   return (
-    <div className="App text-slate-300 bg-slate-900 font-sans min-h-screen min-w-screen font-light">
-        <section className='flex flex-col items-center justify-center p-2 m-0 gap-4'>
-          <h1 className='text-4xl animate-pulse text-'>Portfolio Under Maintenance</h1>
-          <h3 className='font-mono'>This is the way</h3>
-          <iframe src="https://giphy.com/embed/xUPJPdEApVwRVwnPIA" height="450" width="400" className="rounded-lg object-cover" class="giphy-embed" allowFullScreen title="bob the builder"></iframe>
-        </section>
-      </div>
+    <div>
+      Hello There
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,56 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],  theme: {
-    fontFamily: {
-      'sans' : ['Poppins', 'sans-serif'],
-      'mono' : ['Jost', 'sans-serif']
+    fontFamily : {
+      regular : ['Inter', 'sans-serif'],
     },
-    extend: {},
+    fontSize : {
+      'heading' : ['36px', {
+          lineHeight : '40px',
+          letterSpacing : '0px',
+          fontWeight : '800',
+        }
+      ],
+    'section' : ['24px', {
+        lineHeight : '32px',
+        letterSpacing : '0px',
+        fontWeight : '600',
+      }
+     ],
+   'button' : ['20px', {
+        lineHeight : '28px',
+        letterSpacing : '0px',
+        fontWeight : '500',
+      }
+    ],
+    'regular' : ['18px', {
+        lineHeight : '28px',
+        letterSpacing : '0px',
+        fontWeight : '400',
+      }
+    ],
   },
-  plugins: [],
+    screens: {
+      'mobile' : '375px',
+      'tablet': '640px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+    },
+
+    colors : {
+      'navy' : '#0a192f',
+      'slate' : '#8892b0',
+      'white' : '#e6f1ff',
+      'green' : '#64ffda',
+      'light-navy' : '#112240',
+      'light-slate' : '#a8b2d1',
+      'lightest-navy' : '#233554',
+      'lightest-slate' : '#ccd6f6',
+    },
+    extend: {
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
